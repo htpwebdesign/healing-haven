e098453fbb3bd9c792bed2caa3a591cf31d12cf5
Brice Chen <bchen72@my.bcit.ca>
2023-06-28 10:12:41 -0700

    added code to output page-contact

--- page-contact.php
+++ page-contact.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying all contact page
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -18,15 +18,73 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<?php
+
 		while ( have_posts() ) :
+			?>
+		<?php
+		$contact_field = get_field('contact_form_intro');
+		$phone_number = get_field('phone_number');
+		$clinic_email = get_field('clinic_email');
+		$clinic_address = get_field('address');
+		$google_map = get_field('address_map');
+			
+		if ($phone_number) {
+			?>
+			<div class="contact-form-phone">
+				<?php echo $phone_number; ?>
+			</div>
+			<?php
+		}
+		?>
+		<?php
+		if ($clinic_email) {
+			?>
+			<div class="contact-form-email">
+				<?php echo $clinic_email; ?>
+			</div>
+			<?php
+		}
+		?>
+		<?php
+		if ($clinic_address) {
+			?>
+			<div class="contact-form-address">
+				<?php echo $clinic_address; ?>
+			</div>
+			<?php
+		}
+		?>
+		<?php
+		if ($google_map) {
+			?>
+			<div class="contact-form-map">
+				<?php echo $google_map; ?>
+			</div>
+			<?php
+		}
+		?>
+		<?php
+		if ($contact_field) {
+			?>
+			<div class="contact-form-intro">
+				<?php echo $contact_field; ?>
+			</div>
+			<?php
+		}
+		?>
+		<?php
+		if ($contact_field) {
+			?>
+			<div class="contact-form-intro">
+				<?php echo $contact_field; ?>
+			</div>
+			<?php
+		}
+		?>
+		<?php
 			the_post();
 
 			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
 
 		endwhile; // End of the loop.
 		?>
@@ -34,5 +92,4 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
