536ab2e1c8e02be662d2dd3110c84c878ee1fc63
Crystal Chen <cchen435@my.bcit.ca>
2023-06-28 16:08:41 -0700

    created template part for popular services

--- front-page.php
+++ front-page.php
@@ -62,31 +62,7 @@ get_header();
 
 				<section class="home-popular-services">
 					<h2>Popular Services</h2>
-					<?php 
-					if ( function_exists( 'get_field' ) ) {
-						if ( get_field( 'popular_services' ) ) { 
-							$popular_services = get_field('popular_services'); ?>
-
-							<ul>
-							<?php foreach( $popular_services as $popular_service) {
-								$service = wc_get_product($popular_service);
-								$title = $service->get_title();
-								$thumbnail = $service->get_image('thumbnail');
-								$permalink = $service->get_permalink();
-								?>
-								<li>
-									<?php echo $thumbnail ?>
-									<?php echo $title ?>
-									<a href="<?php echo esc_url($permalink) ?>">
-										<p>More Info</p>
-									</a>
-								</li>
-							<?php } ?>
-							</ul>
-						<?php
-						}
-					}
-					?>
+					<?php get_template_part( 'template-parts/popular', 'services' ); ?>
 				</section>
 
 				<section class="home-testimonials">
--- single.php
+++ single.php
@@ -24,7 +24,11 @@ get_header();
 					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'healing-haven' ) . '</span> <span class="nav-title">%title</span>',
 				)
 			);
+			?>
 
+
+
+			<?php
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
@@ -32,9 +36,13 @@ get_header();
 
 		endwhile; // End of the loop.
 		?>
+	
+		<!-- Display Popular services -->
+		<div>
+			<?php get_template_part( 'template-parts/popular', 'services' ); ?>
+		</div>
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
