0db694961933fc6a913ee34b2d2c406459db57bc
fbieks <fbieker@hotmail.com>
2023-07-04 14:24:26 -0700

    created footer menu

--- footer.php
+++ footer.php
@@ -13,17 +13,9 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'healing-haven' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'healing-haven' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'healing-haven' ), 'healing-haven', '<a href="https://healinghaven.bcitwebdeveloper.ca/">FWD 33</a>' );
-				?>
+
+		<?php wp_nav_menu(array('theme_location' => 'footer-right'));?>
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
