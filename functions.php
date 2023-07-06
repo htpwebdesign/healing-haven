ded3e2125abddd4c70607e4ff6e4792a3c29243e
Crystal Chen <cchen435@my.bcit.ca>
2023-07-06 09:41:58 -0700

    created files for lightgallery

--- functions.php
+++ functions.php
@@ -162,7 +162,21 @@ function healing_haven_scripts() {
 		array('jquery'), 
 		'5.8.6', 
 		true);
+	
+	// Enqueue lightgallery scripts on lightbox page
+	if ( is_page(268) ) {
+		wp_enqueue_style( 'lightgallery-styles', get_template_directory_uri() . '/lightgallery/scss/lightgallery-bundle.scss', array(), '2.7.1');
+		wp_enqueue_script( 
+			'lightgallery-settings', 
+			get_template_directory_uri() .'/lightgallery/js/lightgallery.umd.js', 
+			array(), 
+			'2.7.2-beta.1', 
+			true
+		);
+		wp_enqueue_script( 'lightgallery-thumbnail', get_template_directory_uri() .'/lightgallery/js/plugins/thumbnail/lg-thumbnail.umd.js', array('lightgallery-settings'), '2.7.2-beta.1', true);
+		wp_enqueue_script( 'lightgallery-script', get_template_directory_uri() .'/lightgallery/js/lightgallery.js', array('lightgallery-settings'), _S_VERSION, true);
 	}
+}
 add_action( 'wp_enqueue_scripts', 'healing_haven_scripts' );
 
 
---
+++ page-lightbox.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Healing_Haven_Massage
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php
+					if ( is_singular() ) :
+						the_title( '<h1 class="entry-title">', '</h1>' );
+					else :
+						the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+					endif;
+					?>
+				</header><!-- .entry-header -->
+
+				<div class="entry-content">
+					<?php 
+					if ( function_exists( 'get_field' ) ) {
+						if ( get_field( 'image_gallery' ) ) {
+							$imageGallery = get_field('image_gallery'); ?>
+							<div id="lightgallery">
+							<?php
+							foreach( $imageGallery as $image ) { ?>
+								<a><?php echo wp_get_attachment_image($image); ?></a>
+								<?php
+							} ?>
+
+							</div>
+							<?php
+						};
+					};
+					?>
+
+				</div><!-- .entry-content -->
+
+			</article><!-- #post-<?php the_ID(); ?> -->
+
+			<?php
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
