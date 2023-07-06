7d3fd5730ee95cbc821df9e728120bf13a643e0c
Yui Sakamoto <yui.sakamoto11@gmail.com>
2023-07-06 14:00:37 -0700

    changed layout for blog posts page

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,7 +10,10 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+
+	<?php healing_haven_post_thumbnail(); ?>
+
+	<div class="text-contents">
 		<?php
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
@@ -31,41 +34,35 @@
 				?>
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
-	</header><!-- .entry-header -->
 
-	<?php healing_haven_post_thumbnail(); ?>
+		<div class="entry-content">
+			<?php
+			if( is_single() ){
+				the_content(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'healing-haven' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						wp_kses_post( get_the_title() )
+					)
+				);
+			} else {
+				the_excerpt();
+			}
 
-	<div class="entry-content">
-		<?php
-		if( is_single() ){
-			the_content(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'healing-haven' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'healing-haven' ),
+					'after'  => '</div>',
 				)
 			);
-		} else {
-			the_excerpt();
-		}
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'healing-haven' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php healing_haven_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+			?>
+		</div><!-- .entry-content -->
+	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
