3242790a82429c96a47809357493df1f48f1b1f4
Yui Sakamoto <yui.sakamoto11@gmail.com>
2023-07-11 16:01:08 -0700

    added some styles to checkout page

---
+++ page-checkout.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * The template for displaying Checkout Page.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Healing_Haven_Massage
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+		?>
+
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="entry-header">
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			</header><!-- .entry-header -->
+
+			<?php healing_haven_post_thumbnail(); ?>
+
+			<div class="entry-content">
+				<?php
+				the_content();
+				?>
+			</div><!-- .entry-content -->
+
+			<?php if ( get_edit_post_link() ) : ?>
+				<footer class="entry-footer">
+					<?php
+					edit_post_link(
+						sprintf(
+							wp_kses(
+								/* translators: %s: Name of current post. Only visible to screen readers */
+								__( 'Edit <span class="screen-reader-text">%s</span>', 'healing-haven' ),
+								array(
+									'span' => array(
+										'class' => array(),
+									),
+								)
+							),
+							wp_kses_post( get_the_title() )
+						),
+						'<span class="edit-link">',
+						'</span>'
+					);
+					?>
+				</footer><!-- .entry-footer -->
+			<?php endif; ?>
+		</article><!-- #post-<?php the_ID(); ?> -->
+		<?php
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
