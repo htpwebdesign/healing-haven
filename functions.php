c9d09ad358ade39690bd6f948959c760636cd0c5
fbieks <fbieker@hotmail.com>
2023-07-18 12:33:38 -0700

    Styled Wordpress Login Page

--- functions.php
+++ functions.php
@@ -432,3 +432,20 @@ function hhm_post_filter( $use_block_editor, $post ) {
     }
 }
 add_filter( 'use_block_editor_for_post', 'hhm_post_filter', 10, 2 );
+
+
+//Styling Wordpress Login
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'Healing Haven Massage';
+}
+add_filter( 'login_headertext', 'my_login_logo_url_title' );
+
+function my_login_stylesheet() {
+    wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/css/style-login.css' );
+}
+add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
